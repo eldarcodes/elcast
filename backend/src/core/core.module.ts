dbf6af501cc97e440531d5ac1e7f8c77e4253058
eldarcodes <prog.eldar@gmail.com>
2025-01-02 12:18:05 +0200

    feat: add config module, is-dev utils

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { IS_DEV_ENV } from '../shared/utils/is-dev.util';
+
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: !IS_DEV_ENV,
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
 })
 export class CoreModule {}
