ff5bdf740419a9319b6067c57efaeaba430aecb2
eldarcodes <prog.eldar@gmail.com>
2025-01-06 17:55:47 +0200

    feat: implement storage module with S3 integration for file uploads and deletions

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -11,6 +11,7 @@ import { TotpModule } from '../modules/auth/totp/totp.module';
 import { VerificationModule } from '../modules/auth/verification/verification.module';
 import { CronModule } from '../modules/cron/cron.module';
 import { MailModule } from '../modules/libs/mail/mail.module';
+import { StorageModule } from '../modules/libs/storage/storage.module';
 import { IS_DEV_ENV } from '../shared/utils/is-dev.util';
 
 import { getGraphQLConfig } from './config/graphql.config';
@@ -34,6 +35,7 @@ import { RedisModule } from './redis/redis.module';
     RedisModule,
     MailModule,
     CronModule,
+    StorageModule,
 
     AccountModule,
     SessionModule,
