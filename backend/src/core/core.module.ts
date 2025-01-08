d27e96323efa69a96aa3b680432edcafe423e233
eldarcodes <prog.eldar@gmail.com>
2025-01-08 12:46:18 +0200

    feat: integrate LiveKit SDK with dynamic module and service implementation

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -11,12 +11,14 @@ import { SessionModule } from '../modules/auth/session/session.module';
 import { TotpModule } from '../modules/auth/totp/totp.module';
 import { VerificationModule } from '../modules/auth/verification/verification.module';
 import { CronModule } from '../modules/cron/cron.module';
+import { LivekitModule } from '../modules/libs/livekit/livekit.module';
 import { MailModule } from '../modules/libs/mail/mail.module';
 import { StorageModule } from '../modules/libs/storage/storage.module';
 import { StreamModule } from '../modules/stream/stream.module';
 import { IS_DEV_ENV } from '../shared/utils/is-dev.util';
 
 import { getGraphQLConfig } from './config/graphql.config';
+import { getLivekitConfig } from './config/livekit.config';
 import { PrismaModule } from './prisma/prisma.module';
 import { RedisModule } from './redis/redis.module';
 
@@ -38,6 +40,11 @@ import { RedisModule } from './redis/redis.module';
     MailModule,
     CronModule,
     StorageModule,
+    LivekitModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: getLivekitConfig,
+      inject: [ConfigService],
+    }),
 
     AccountModule,
     SessionModule,
