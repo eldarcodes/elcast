29746f3b689d930f54cdce61ed60fbccc94b2538
eldarcodes <prog.eldar@gmail.com>
2025-01-09 15:47:40 +0200

    feat: add follow and unfollow logic

---
+++ backend/src/modules/follow/follow.service.ts
@@ -0,0 +1,119 @@
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { User } from '@/prisma/generated';
+import { PrismaService } from '@/src/core/prisma/prisma.service';
+
+@Injectable()
+export class FollowService {
+  public constructor(private readonly prismaService: PrismaService) {}
+
+  public async findMyFollowers(user: User) {
+    const followers = await this.prismaService.follow.findMany({
+      where: {
+        followingId: user.id,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        follower: true,
+      },
+    });
+
+    return followers;
+  }
+
+  public async findMyFollowings(user: User) {
+    const following = await this.prismaService.follow.findMany({
+      where: {
+        followerId: user.id,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        following: true,
+      },
+    });
+
+    return following;
+  }
+
+  public async follow(user: User, channelId: string) {
+    const channel = await this.prismaService.user.findUnique({
+      where: {
+        id: channelId,
+      },
+    });
+
+    if (!channel) {
+      throw new NotFoundException('Channel not found');
+    }
+
+    if (channel.id === user.id) {
+      throw new ConflictException('You cannot follow yourself');
+    }
+
+    const existingFollow = await this.prismaService.follow.findFirst({
+      where: {
+        followerId: user.id,
+        followingId: channelId,
+      },
+    });
+
+    if (existingFollow) {
+      throw new ConflictException('You are already following this channel');
+    }
+
+    await this.prismaService.follow.create({
+      data: {
+        followerId: user.id,
+        followingId: channelId,
+      },
+    });
+
+    return true;
+  }
+
+  public async unfollow(user: User, channelId: string) {
+    const channel = await this.prismaService.user.findUnique({
+      where: {
+        id: channelId,
+      },
+    });
+
+    if (!channel) {
+      throw new NotFoundException('Channel not found');
+    }
+
+    if (channel.id === user.id) {
+      throw new ConflictException('You cannot unfollow yourself');
+    }
+
+    const existingFollow = await this.prismaService.follow.findFirst({
+      where: {
+        followerId: user.id,
+        followingId: channelId,
+      },
+    });
+
+    if (!existingFollow) {
+      throw new ConflictException('You are not following this channel');
+    }
+
+    await this.prismaService.follow.delete({
+      where: {
+        id: existingFollow.id,
+        followerId: user.id,
+        followingId: channelId,
+      },
+    });
+
+    return true;
+  }
+}
