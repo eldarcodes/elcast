02ec8a116df8fbdce0579856793c8ecf19e172e1
eldarcodes <prog.eldar@gmail.com>
2025-01-09 17:49:49 +0200

    feat: integrate NotificationService into Follow and Webhook modules

--- backend/src/modules/follow/follow.module.ts
+++ backend/src/modules/follow/follow.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { NotificationModule } from '../notification/notification.module';
+import { NotificationService } from '../notification/notification.service';
 
 import { FollowResolver } from './follow.resolver';
 import { FollowService } from './follow.service';
 
 @Module({
-  providers: [FollowResolver, FollowService, NotificationModule],
+  providers: [FollowResolver, FollowService, NotificationService],
 })
 export class FollowModule {}
