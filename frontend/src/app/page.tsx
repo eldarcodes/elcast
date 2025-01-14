ff6eeabb4b9a6796f59ecfef7015c7645c6b0b00
eldarcodes <prog.eldar@gmail.com>
2025-01-14 15:25:21 +0200

    feat: add base button and change primary theme

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,17 @@
 
 import { useTranslations } from 'next-intl';
 
+import { Button } from '@/components/ui/common/button';
+
 export default function Home() {
   const t = useTranslations('home');
 
-  return <div>{t('hello')}</div>;
+  return (
+    <div className="h-[2000px]">
+      <Button variant="default">{t('hello')}</Button>
+      <Button variant="outline">{t('hello')}</Button>
+      <Button variant="secondary">{t('hello')}</Button>
+      <Button variant="ghost">{t('hello')}</Button>
+    </div>
+  );
 }
