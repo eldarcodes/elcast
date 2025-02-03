c53a7e587ea961eb469cfa98e2d44238479b98c7
eldarcodes <prog.eldar@gmail.com>
2025-02-03 15:24:42 +0200

    feat: add leaflet map to session modal

--- frontend/src/components/features/user/sessions/session-modal.tsx
+++ frontend/src/components/features/user/sessions/session-modal.tsx
@@ -2,10 +2,13 @@
 
 import dayjs from 'dayjs';
 import { useTranslations } from 'next-intl';
+import dynamic from 'next/dynamic';
 
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
+  DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/common/dialog';
@@ -15,6 +18,11 @@ import { FindSessionsByUserQuery } from '@/graphql/generated/output';
 interface SessionModalProps {
   session: FindSessionsByUserQuery['findSessionsByUser'][0];
 }
+
+const MapLeaflet = dynamic(
+  () => import('@/components/ui/elements/leaflet-map'),
+  { ssr: false },
+);
 
 export function SessionModal({
   session,
@@ -29,7 +37,10 @@ export function SessionModal({
       <DialogTrigger asChild>{children}</DialogTrigger>
 
       <DialogContent>
-        <DialogTitle className="text-xl">{t('heading')}</DialogTitle>
+        <DialogHeader>
+          <DialogTitle className="text-xl">{t('heading')}</DialogTitle>
+          <DialogDescription>Session ID: {session.id}</DialogDescription>
+        </DialogHeader>
         <div className="space-y-3">
           <div className="flex items-center">
             <span className="font-medium">{t('device')}</span>
@@ -62,6 +73,11 @@ export function SessionModal({
               {dayjs(session.createdAt).format('DD MMMM YYYY, HH:mm')}
             </span>
           </div>
+
+          <MapLeaflet
+            latitude={session.metadata.location.latitude}
+            longitude={session.metadata.location.longitude}
+          />
         </div>
       </DialogContent>
     </Dialog>
