dc03963fc4b6202ae2b104ef81f60de0d0ec0b33
eldarcodes <prog.eldar@gmail.com>
2025-03-21 14:54:43 +0200

    feat(apps): use pnpm as package manager

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,9 +8,6 @@ const parsedUrl = new URL(s3Url);
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
