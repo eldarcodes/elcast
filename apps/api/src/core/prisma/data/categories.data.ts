d9a0a2f4c100c27ddd7f9617e4d0b39ea01ae575
eldarcodes <prog.eldar@gmail.com>
2025-04-07 19:57:12 +0300

    feat(seeder): add tags for categories

--- apps/api/src/core/prisma/data/categories.data.ts
+++ apps/api/src/core/prisma/data/categories.data.ts
@@ -3,13 +3,14 @@ export const CATEGORIES = [
     title: 'Just Chatting',
     slug: 'just-chatting',
     thumbnailUrl: '/categories/just-chatting.webp',
-    tags: [],
+    tags: ['IRL'],
     description: '',
   },
   {
     title: 'Counter-Strike',
     slug: 'counter-strike',
     thumbnailUrl: '/categories/counter-strike.webp',
+    tags: ['FPS', 'Shooter', 'Action'],
     description:
       'Counter-Strike has been the pioneering tactical shooter since the franchise debuted in November 2000. This legacy continues with Counter-Strike: Global Offensive and the upcoming release of CS2.',
   },
@@ -17,7 +18,7 @@ export const CATEGORIES = [
     title: 'VALORANT',
     slug: 'valorant',
     thumbnailUrl: '/categories/valorant.webp',
-    tags: [],
+    tags: ['FPS', 'Shooter', 'Action'],
     description:
       'Valorant is a character-based 5v5 tactical shooter set on the global stage. Outwit, outplay, and outshine your competition with tactical abilities, precise gunplay, and adaptive teamwork.',
   },
@@ -25,7 +26,7 @@ export const CATEGORIES = [
     title: 'World of Warcraft',
     slug: 'world-of-warcraft',
     thumbnailUrl: '/categories/world-of-warcraft.webp',
-    tags: [],
+    tags: ['RPG', 'Adventure Game', 'MMO', 'Action', 'Open World'],
     description:
       'World of Warcraft is a massively multiplayer online roleplaying game (MMORPG) set in the Warcraft universe. Players assume the roles of Warcraft heroes as they explore, adventure, and quest across a vast world. Being "Massively Multiplayer," World of Warcraft allows thousands of players to interact within the same world. Whether adventuring together or fighting against each other in epic battles, players will form friendships, forge alliances, and compete with enemies for power and glory.',
   },
@@ -33,7 +34,7 @@ export const CATEGORIES = [
     title: 'League of Legends',
     slug: 'league-of-legends',
     thumbnailUrl: '/categories/league-of-legends.webp',
-    tags: [],
+    tags: ['RPG', 'Strategy', 'MOBA', 'Action'],
     description:
       'League of Legends is a fast-paced, competitive online game that blends the speed and intensity of an RTS with RPG elements. Two teams of powerful champions, each with a unique design and playstyle, battle head-to-head across multiple battlefields and game modes. With an ever-expanding roster of champions, frequent updates and a thriving tournament scene, League of Legends offers endless replayability for players of every skill level.',
   },
@@ -41,7 +42,7 @@ export const CATEGORIES = [
     title: 'Grand Theft Auto V',
     slug: 'grand-theft-auto-v',
     thumbnailUrl: '/categories/grand-theft-auto-v.webp',
-    tags: [],
+    tags: ['Action', 'Adventure Game', 'Driving/Racing Game'],
     description:
       'Grand Theft Auto V is a vast open world game set in Los Santos, a sprawling sun-soaked metropolis struggling to stay afloat in an era of economic uncertainty and cheap reality TV. The game blends storytelling and gameplay in new ways as players repeatedly jump in and out of the lives of the game’s three lead characters, playing all sides of the game’s interwoven story.',
   },
@@ -49,7 +50,7 @@ export const CATEGORIES = [
     title: 'Fortnite',
     slug: 'fortnite',
     thumbnailUrl: '/categories/fortnite.webp',
-    tags: [],
+    tags: ['Shooter', 'Battle Royale', 'RPG', 'Action', 'FPS', 'Strategy'],
     description:
       'Fortnite is the completely free online game where you and your friends fight to be the last one standing in Battle Royale, join forces to make your own Creative games, or catch a live show at Party Royale.',
   },
@@ -57,7 +58,7 @@ export const CATEGORIES = [
     title: 'Dota 2',
     slug: 'dota-2',
     thumbnailUrl: '/categories/dota-2.webp',
-    tags: [],
+    tags: ['Strategy', 'MOBA', 'Action'],
     description:
       'Dota 2 is a multiplayer online battle arena video game and the stand-alone sequel to the Defense of the Ancients (DotA) mod. With regular updates that ensure a constant evolution of gameplay, features, and heroes, Dota 2 has taken on a life of its own.',
   },
@@ -65,7 +66,7 @@ export const CATEGORIES = [
     title: 'EA Sports FC 25',
     slug: 'ea-sports-fc-25',
     thumbnailUrl: '/categories/ea-sports-fc-25.webp',
-    tags: [],
+    tags: ['Simulation', 'Sports Game'],
     description:
       'EA SPORTS FC 25 gives you more ways to win for the club. Team up with friends in your favourite modes with the new 5v5 Rush, and manage your club to victory as FC IQ delivers more tactical control than ever before.',
   },
@@ -73,7 +74,7 @@ export const CATEGORIES = [
     title: 'Minecraft',
     slug: 'minecraft',
     thumbnailUrl: '/categories/minecraft.webp',
-    tags: [],
+    tags: ['Simulation', 'Adventure Game', 'MMO', 'Action', 'Survival'],
     description:
       "Minecraft focuses on allowing the player to explore, interact with, and modify a dynamically-generated map made of one-cubic-meter-sized blocks. In addition to blocks, the environment features plants, mobs, and items. Some activities in the game include mining for ore, fighting hostile mobs, and crafting new blocks and tools by gathering various resources found in the game. The game's open-ended model allows players to create structures, creations, and artwork on various multiplayer servers or their single-player maps. Other features include redstone circuits for logic computations and remote actions, minecarts and tracks, and a mysterious underworld called the Nether. A designated but completely optional goal of the game is to travel to a dimension called the End, and defeat the ender dragon.",
   },
@@ -81,7 +82,7 @@ export const CATEGORIES = [
     title: 'Elden Ring',
     slug: 'elden-ring',
     thumbnailUrl: '/categories/elden-ring.webp',
-    tags: [],
+    tags: ['RPG', 'Adventure Game', 'Action', 'Open World'],
     description:
       'Elden Ring is an action RPG developed by FromSoftware and published by Bandai Namco Entertainment, released in February 2022. Directed by Hidetaka Miyazaki, with world-building contributions from novelist George R. R. Martin, the game features an expansive open world called the Lands Between. Players assume the role of a customisable character known as the Tarnished, who must explore this world, battle formidable enemies, and seek to restore the Elden Ring to become the Elden Lord. The game builds on the challenging gameplay mechanics familiar from the Dark Souls series but introduces a more open-ended structure with vast exploration, dynamic weather, and a day-night cycle. It offers deep lore, complex characters, and an interconnected world filled with secrets, dungeons, and powerful bosses.',
   },
@@ -89,14 +90,14 @@ export const CATEGORIES = [
     title: 'IRL',
     slug: 'irl',
     thumbnailUrl: '/categories/irl.webp',
-    tags: [],
+    tags: ['IRL'],
     description: '',
   },
   {
     title: "Tom Clancy's Rainbow Six Siege",
     slug: 'tom-clancys-rainbow-six-siege',
     thumbnailUrl: '/categories/tom-clancys-rainbow-six-siege.webp',
-    tags: [],
+    tags: ['FPS', 'Shooter', 'Action'],
     description:
       'Inspired by the reality of counter terrorist operatives across the world, Rainbow Six Siege invites players to master the art of destruction. Intense close quarters confrontations, high lethality, tactics, team play, and explosive action are at the center of the experience. The multiplayer gameplay of Rainbow Six Siege sets a new bar for intense firefights and expert strategy in the rich legacy of past Rainbow Six games.',
   },
@@ -104,7 +105,7 @@ export const CATEGORIES = [
     title: 'Hearthstone',
     slug: 'hearthstone',
     thumbnailUrl: '/categories/hearthstone.webp',
-    tags: [],
+    tags: ['Strategy', 'Cards & Board Game'],
     description:
       "Sheathe your sword, draw your deck, and get ready for Hearthstone - the fast paced strategy card game that's easy to learn and massively fun. Start a free game on Battle.net and play your cards to sling spells, summon creatures, and command the heroes of Warcraft in duels of epic strategy. With powerful prebuilt decks, and hundreds of additional cards to win, craft or purchase - your collection never stops evolving. Challenge players of all skill levels on Battle.net or hone your skills in practice matches against some of the greatest strategists of Azeroth - Thrall, Uther, Gul'dan, and more!",
   },
@@ -112,7 +113,7 @@ export const CATEGORIES = [
     title: 'DayZ',
     slug: 'dayz',
     thumbnailUrl: '/categories/dayz.webp',
-    tags: [],
+    tags: ['FPS', 'Shooter', 'RPG', 'Simulation', 'Survival'],
     description:
       'DayZ is a hardcore open-world survival game with only one rule: stay alive, no matter what. How long can you survive a post-apocalyptic world? A land overrun with an infected "zombie" population, where you compete with other survivors for limited resources. Will you team up with strangers and stay strong together? Or play as a lone wolf to avoid betrayal? This is DayZ, this is your story.',
   },
@@ -120,7 +121,7 @@ export const CATEGORIES = [
     title: 'GeoGuessr',
     slug: 'geoguessr',
     thumbnailUrl: '/categories/geoguessr.webp',
-    tags: [],
+    tags: ['Strategy', 'Educational Game'],
     description:
       'A game of simulated world travel and geographical knowledge. Players must pinpoint their location on the map after being placed in a random Street View location.',
   },
@@ -128,14 +129,14 @@ export const CATEGORIES = [
     title: 'Chess',
     slug: 'chess',
     thumbnailUrl: '/categories/chess.webp',
-    tags: [],
+    tags: ['Strategy', 'Cards & Board Game'],
     description: '',
   },
   {
     title: 'Cyberpunk 2077',
     slug: 'cyberpunk-2077',
     thumbnailUrl: '/categories/cyberpunk-2077.webp',
-    tags: [],
+    tags: ['FPS', 'Shooter', 'RPG', 'Adventure Game', 'Open World'],
     description:
       'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality. You can customize your character’s cyberware, skillset and playstyle, and explore a vast city where the choices you make shape the story and the world around you.',
   },
@@ -143,7 +144,7 @@ export const CATEGORIES = [
     title: 'Detroit: Become Human',
     slug: 'detroit-become-human',
     thumbnailUrl: '/categories/detroit-become-human.webp',
-    tags: [],
+    tags: ['Puzzle', 'Adventure Game', 'Action', 'Mystery'],
     description:
       'Detroit: Become Human puts the destiny of both mankind and androids in your hands, taking you to a near future where machines have become more intelligent than humans. Every choice you make affects the outcome of the game, with one of the most intricately branching narratives ever created.',
   },
@@ -151,14 +152,14 @@ export const CATEGORIES = [
     title: 'Software and Game Development',
     slug: 'software-and-game-development',
     thumbnailUrl: '/categories/software-and-game-development.webp',
-    tags: [],
+    tags: ['Creative'],
     description: '',
   },
   {
     title: 'Rocket League',
     slug: 'rocket-league',
     thumbnailUrl: '/categories/rocket-league.webp',
-    tags: [],
+    tags: ['Driving/Racing Game', 'Sports Game', 'Action'],
     description:
       'Rocket League is a high-powered hybrid of arcade-style soccer and vehicular mayhem with easy-to-understand controls and fluid, physics-driven competition. Rocket League includes casual and competitive Online Matches, a fully-featured offline Season Mode, special “Mutators” that let you change the rules entirely, hockey and basketball-inspired Extra Modes, and more than 500 trillion possible cosmetic customization combinations.',
   },
@@ -166,7 +167,7 @@ export const CATEGORIES = [
     title: 'Marvel Rivals',
     slug: 'marvel-rivals',
     thumbnailUrl: '/categories/marvel-rivals.webp',
-    tags: [],
+    tags: ['Action', 'Shooter'],
     description:
       'Marvel Rivals is an hero shooter video game developed and published by NetEase Games in collaboration with Marvel Games. The game is free-to-play with a current line up of 33 characters from Marvel Comics.',
   },
@@ -174,7 +175,7 @@ export const CATEGORIES = [
     title: 'Rust',
     slug: 'rust',
     thumbnailUrl: '/categories/rust.webp',
-    tags: [],
+    tags: ['FPS', 'Shooter', 'RPG', 'Simulation', 'Survival'],
     description:
       'The only aim in Rust is to survive. Everything wants you to die - the island’s wildlife and other inhabitants, the environment, other survivors. Do whatever it takes to last another night.',
   },
@@ -182,7 +183,7 @@ export const CATEGORIES = [
     title: 'PUBG: BATTLEGROUNDS',
     slug: 'pubg-battlegrounds',
     thumbnailUrl: '/categories/pubg-battlegrounds.webp',
-    tags: [],
+    tags: ['FPS', 'Shooter', 'Battle Royale', 'Action'],
     description:
       'PUBG: Battlegrounds is a battle royale shooter that pits 100 players against each other in a struggle for survival. Gather supplies and outwit your opponents to become the last person standing.',
   },
@@ -190,7 +191,7 @@ export const CATEGORIES = [
     title: 'Apex Legends',
     slug: 'apex-legends',
     thumbnailUrl: '/categories/apex-legends.webp',
-    tags: [],
+    tags: ['FPS', 'Shooter', 'Battle Royale', 'Action'],
     description:
       'Conquer with character in Apex Legends, a free-to-play Hero shooter where legendary characters with powerful abilities team up to battle for fame & fortune on the fringes of the Frontier. Master an ever-growing roster of diverse Legends, deep tactical squad play and bold new innovations that go beyond the Battle Royale experience—all within a rugged world where anything goes. Welcome to the next evolution of Hero Shooter.',
   },
