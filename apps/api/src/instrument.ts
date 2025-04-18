00e31b14c491b52ebbd3ba1845d82834b9449586
eldarcodes <prog.eldar@gmail.com>
2025-04-18 14:11:23 +0300

    fix: disable sentry for development

--- apps/api/src/instrument.ts
+++ apps/api/src/instrument.ts
@@ -4,6 +4,8 @@ const { nodeProfilingIntegration } = require('@sentry/profiling-node');
 Sentry.init({
   dsn: 'https://5d7f9493a5fb23b1c7013d77bc90d7b0@o599309.ingest.us.sentry.io/4509168535928832',
 
+  enabled: process.env.NODE_ENV === 'production',
+
   integrations: [
     // Add our Profiling integration
     nodeProfilingIntegration(),
